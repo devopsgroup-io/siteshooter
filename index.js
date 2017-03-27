7133de6e27ebfe08c2e96e44ec4ae7ed7d003653
Steve Britton <stvnbritton@gmail.com>
2017-03-27 17:33:09 -0400

    chore(cleanup)

--- index.js
+++ index.js
@@ -1,27 +1,3 @@
 'use strict';
-
-var siteshooter = require('./lib/siteshooter'),
-    args = [].slice.call(process.argv, 2),
-    exitCode = 0,
-    isDebug = args.indexOf('--debug') !== -1;
-
-
-module.exports = function() {
-
-    siteshooter.cli(args).then(function() {
-        process.exit(exitCode);
-
-    }).catch(function(err) {
-        exitCode = 1;
-        if (!isDebug) {
-            console.error(err);
-        } else {
-            throw new Error(err);
-        }
-    });
-
-};
-
-process.on('exit', function() {
-    process.exit(exitCode);
-});
+module.exports = {};
+module.exports = require('./lib/siteshooter');
