cea2c6d578bcb4898356d116df023ad43df06cea
Steve Britton <stvnbritton@gmail.com>
2017-03-27 17:34:52 -0400

    feat(cli): verify node version.

--- bin/siteshooter.js
+++ bin/siteshooter.js
@@ -1,10 +1,28 @@
-#!/usr/bin/env node
+#! /usr/bin/env node
+'use strict';
 
-var siteshooter = require('../lib/siteshooter'),
+var chalk = require('chalk'),
+    pkg = require('../package.json');
+
+var nodeVersion = process.version.replace('v',''),
+    nodeVersionRequired = pkg.engines.node.replace('>=','');
+
+// check node version compatibility
+if(nodeVersion <= nodeVersionRequired){
+
+    console.log();
+    console.error(chalk.red.bold('✗ '), chalk.red.bold('Siteshooter requires node version ' + pkg.engines.node));
+    console.log();
+
+    process.exit(1);
+}
+
+var siteshooter = require('../index'),
     args = [].slice.call(process.argv, 2);
 
 var exitCode = 0,
     isDebug = args.indexOf('--debug') !== -1;
+
 
 siteshooter.cli(args).then(function() {
 
